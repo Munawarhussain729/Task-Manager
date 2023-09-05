ee7feeb1736e1d6e873fec768dbbe4fcc305e56f
Munawar Hussain <munawarhussa.7@gmail.com>
2023-09-05 17:07:26 +0500

    Description adn Hide button added

--- src/components/ListTasks.js
+++ src/components/ListTasks.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ListSection from './ListSection'
 import DropDown from './DropDown';
 
@@ -9,6 +9,9 @@ function ListTasks({ tasks, setTasks }) {
     const [isSidebarVisible, setSidebarVisible] = useState(false)
     const [dateValue, setDateValue] = useState(new Date());
     const [priority, setPriority] = useState('')
+    const [description, setDescription] = useState('')
+
+    const textAreaRef = useRef(null)
 
     const handleDateChange = newValue => {
         setDateValue(newValue);
@@ -23,6 +26,10 @@ function ListTasks({ tasks, setTasks }) {
         setclosed(tempClosed)
     }, [tasks])
 
+    useEffect(() => {
+        textAreaRef.current.style.height = "auto"
+        textAreaRef.current.style.height = textAreaRef.current.scrollHeight + "px"
+    }, [description])
     const status = ['toDo', 'inProgress', 'closed']
     return (
         <>
@@ -44,9 +51,12 @@ function ListTasks({ tasks, setTasks }) {
                 ))}
             </div>
 
-            <div className={`bg-slate-300 p-5  min-h-screen absolute rounded-xl text-black right-0 ${isSidebarVisible ? "w-[45rem] visible" : "w-0 invisible "} overflow-x-hidden transition-all duration-300 ease-in-out`}>
-                <button className=" top-2 right-2" onClick={() => { setSidebarVisible(!isSidebarVisible) }}>
-                    {isSidebarVisible ? "Hide" : "Show"} Sidebar
+            <div
+                className={`bg-slate-300 p-5  min-h-screen absolute rounded-xl text-black right-0 
+            ${isSidebarVisible ? "w-[45rem] visible" : "w-0 invisible "} overflow-x-hidden transition-all 
+            duration-300 ease-in-out `}>
+                <button className=" top-2 right-2 p-2 bg-gray-50 hover:bg-gray-600 hover:text-white w-40 rounded-lg" onClick={() => { setSidebarVisible(!isSidebarVisible) }}>
+                    Hide Sidebar
                 </button>
                 <hr className='h-1 my-3 bg-slate-400' />
                 <h2 className='text-3xl'>Title</h2>
@@ -65,7 +75,15 @@ function ListTasks({ tasks, setTasks }) {
                 <div className='max-w-[12rem] my-3'>
                     <DropDown value={priority} setValue={setPriority} options={['High', 'medium', 'low']} label={'Select Task priority'} />
                 </div>
-
+                <div >
+                    <h3>Description:</h3>
+                    <textarea
+                        value={description}
+                        onChange={(e) => { setDescription(e.target.value) }}
+                        ref={textAreaRef}
+                        className='p-2 w-full rounded-lg focus:outline-gray-400'
+                        rows={3} />
+                </div>
             </div>
 
 
